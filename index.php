80375329c76c0c89efd4e65d436840f6752fed87
Ken (Nguyen Dinh) Yu <43653630+kendinh216@users.noreply.github.com>
2020-06-11 12:50:49 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Delicious Recipes App</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <h1>Delicious Recipes App</h1>
+
+    <ul>
+      <li>
+        <a href="create.php"><strong>Create New Recipe</strong></a>
+      </li>
+      <li>
+        <a href="read.php"><strong>Filter Recipe</strong></a>
+      </li>
+    </ul>
+  </body>
+</html>
