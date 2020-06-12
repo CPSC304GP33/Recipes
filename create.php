58c8458c9d431fb66c57f66a2daf3547349e2453
jessizhan <66644980+jessizhan@users.noreply.github.com>
2020-06-12 00:44:21 -0700

    Update create.php

--- create.php
+++ create.php
@@ -1,19 +1,55 @@
-<?php include "templates/header.php"; ?><h2>Add a new recipe</h2>
+<?php
+  if (isset($_POST['submit'])) {
+    require "config.php";
+    require "common.php";
 
-<form method="post">
-    <label for="name">Recipe Name</label>
-    <input type="text" name="name" id="name">
-    <label for="skill">Skill Level (easy, medium, hard)</label>
-    <input type="text" name="skill" id="skill">
-    <label for="preptime">PrepTime (minutes)</label>
-    <input type="text" name="preptime" id="preptime">
-    <label for="cooktime">CookTime (minutes)</label>
-    <input type="text" name="cooktime" id="cooktime">
-    <label for="location">Instructions</label>
-    <input type="text" name="instructions" id="instructions">
-    <input type="submit" name="submit" value="Create">
-</form>
+    try {
+      $connection = new PDO($dsn, $username, $password, $options);
 
-<a href="index.php">Back to home</a>
+      $new_user = array(
+        "username" => $_POST['username'],
+        "password" => $_POST['password'],
+        "email" => $_POST['email'],
+        "DietaryRestrictions" => $_POST['diet'],
+        "Preferences" => $_POST['pref']
+      );
+
+      $sql = sprintf(
+        "INSERT INTO %s (%s) values (%s)",
+        "BookUser",
+        implode(",", array_keys($new_user)),
+        ":" . implode(", :", array_keys($new_user))
+      );
+
+      $statement = $connection->prepare($sql);
+      $statement->execute($new_user);
+
+    } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+    }
+  } ?>
+
+  <?php include "templates/header.php"; ?>
+  <?php if (isset($_POST['submit']) && $statement) { ?>
+    <?php echo escape($_POST['username']); ?> successfully added.
+  <?php } ?>
+
+  <h2>Add a user</h2>
+
+  <form method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">Password</label>
+    <input type="text" name="password" id="password">
+    <label for="email">Email Address</label>
+    <input type="text" name="email" id="email">
+    <label for="diet">Dietary Restrictions</label>
+    <input type="text" name="diet" id="diet">
+    <label for="pref">Preferences</label>
+    <input type="text" name="pref" id="pref">
+    <input type="submit" name="submit" value="Submit">
+  </form>
+
+  <a href="index.php">Back to home</a>
 
 <?php include "templates/footer.php"; ?>
