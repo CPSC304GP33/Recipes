bbd79d8cbbef90a60218938b738f636bc5a41d9e
jessizhan <66644980+jessizhan@users.noreply.github.com>
2020-06-12 16:41:54 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,124 @@
+<?php
+//CREDIT: tutorialrepublic.com, bootstrapcdn.com
+
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: index.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+require "common.php";
+
+// Define variables and initialize with empty values
+$username1 = $password1 = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $connection = new PDO($dsn, $username, $password, $options);
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username1 = trim($_POST["username"]);
+    }
+
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password1 = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT username, password FROM BookUser WHERE username = :username";
+
+        if($statement = $connection->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $statement->bindParam(":username", $param_username, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+
+            // Attempt to execute the prepared statement
+            if($statement->execute()){
+                // Check if username exists, if yes then verify password
+                if($statement->rowCount() == 1){
+                    if($row = $statement->fetch()){
+                        $username1 = $row["username"];
+                        $hashed_password = $row["password"];
+                        if($password1 == $hashed_password){
+                            // Password is correct, so start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["username"] = $username1;
+
+                            // Redirect user to welcome page
+                            header("location: index.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            unset($statement);
+        }
+    }
+
+    // Close connection
+    unset($connection);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+  <style type="text/css">
+      body{ font: 14px sans-serif; }
+      .wrapper{ width: 350px; padding: 20px; }
+  </style>
+</head>
+<body>
+  <div class="wrapper">
+      <h2>Login</h2>
+      <p>Please fill in your credentials to login.</p>
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+          <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+              <label>Username</label>
+              <input type="text" name="username" class="form-control" value="<?php echo $username1; ?>">
+              <span class="help-block"><?php echo $username_err; ?></span>
+          </div>
+          <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+              <label>Password</label>
+              <input type="password" name="password" class="form-control">
+              <span class="help-block"><?php echo $password_err; ?></span>
+          </div>
+          <div class="form-group">
+              <input type="submit" class="btn btn-primary" value="Login">
+          </div>
+          <p>Don't have an account? <a href="create.php">Sign up now</a>.</p>
+      </form>
+  </div>
+</body>
+</html>
