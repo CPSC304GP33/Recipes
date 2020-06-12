9b0904e76a4e97fe3c8b5380d9adb92f3a252ede
jessizhan <66644980+jessizhan@users.noreply.github.com>
2020-06-12 00:48:43 -0700

    Add files via upload

---
+++ findUser.php
@@ -0,0 +1,69 @@
+<?php
+  if (isset($_POST['submit'])) {
+    require "config.php";
+    require "common.php";
+
+    try {
+      $connection = new PDO($dsn, $username, $password, $options);
+
+      $sql = "SELECT *
+        FROM BookUser
+        WHERE username = :username";
+
+      $username = $_POST['username'];
+
+      $statement = $connection->prepare($sql);
+      $statement->bindParam(':username', $username, PDO::PARAM_STR);
+      $statement->execute();
+
+      $result = $statement->fetchAll();
+
+    } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+    }
+  } ?>
+
+
+<?php include "templates/header.php" ?>
+
+<?php
+if (isset($_POST['submit'])) {
+  if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+      <thread>
+<tr>
+  <th>Username</th>
+  <th>Email Address</th>
+  <th>Dietary Restrictions</th>
+  <th>Preferences</th>
+</tr>
+    </thead>
+    <tbody>
+  <?php foreach ($result as $row) {?>
+      <tr>
+<td><?php echo escape($row["Username"]); ?></td>
+<td><?php echo escape($row["Email"]); ?></td>
+<td><?php echo escape($row["DietaryRestrictions"]); ?></td>
+<td><?php echo escape($row["Preferences"]); ?></td>
+      </tr>
+    <?php } ?>
+      </tbody>
+  </table>
+  <?php } else { ?>
+    > No results found for <?php echo escape($_POST['username']); ?>.
+  <?php }
+} ?>
+
+  <h2>Find user based on username</h2>
+
+  <form method="post">
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username">
+    <input type="submit" name="submit" value="View Results">
+  </form>
+
+  <a href="index.php">Back to home</a>
+
+<?php include "templates/footer.php" ?>
