a0b6159d929d9a0a835a7c36e476214348f47915
MichauxS <32403126+MichauxSun@users.noreply.github.com>
2020-06-15 04:27:57 -0700

    Add files via upload

---
+++ rate-single.php
@@ -0,0 +1,154 @@
+<?php
+if(session_id() == '') {
+  session_start();
+}
+$username1 = "";
+
+if(!empty($_SESSION['username'])) {
+    $username1 = $_SESSION['username'];
+    echo "Currently logged in as: ". $username1 . "<br>";
+} else {
+    echo 'no session';
+}
+/**
+  * Use an HTML form to edit an entry in the
+  * users table.
+  *
+  */
+require "./config.php";
+require "./common.php";
+if (isset($_POST['submit'])) {
+    try {
+      $connection = new PDO($dsn, $username, $password, $options);
+      $score = $_POST['score'];
+      $reid = $_POST['ReID'];
+      $sql_get_rating = "SELECT * FROM FinalRating WHERE ReID = $reid AND UserName = '" . $_SESSION['username'] . "'";
+      $rating_exist = $connection->prepare($sql_get_rating);
+
+      // $new_score = $_POST["score"];
+      // $sql2 = "INSERT INTO FinalRating (Username, ReID, RID, Score)
+      //          VALUES ('User6',8,6,'".$_POST['score']."')";
+      // $statement = $connection->prepare($sql2);
+      // // $statement->bindValue(':score', $new_score);
+      $rating_exist->execute();
+
+      // echo $username1. "<br>" ;
+      if(!$rating_exist->fetchAll()) {
+          //if RID exist in FinalRating, UPDATE AverageScore, NumOfUsers from RatingResult WHERE FinalRating.RID = RatingResult.RID
+          $sql_rid_command = "SELECT RID FROM FinalRating WHERE ReID = $reid";
+          $sql_rid_value = $connection->prepare($sql_rid_command);
+          $sql_rid_value ->execute();
+
+          if($sql_rid_value->fetchAll()) {
+              $rid = $sql_rid_value->fetchAll()[0]['RID'];
+              echo $rid;
+              $sql_sum_command = "SELECT SUM(Score) AS value_sum FROM FinalRating WHERE ReID = $reid";
+              $sql_sum_value = $connection->prepare($sql_sum_command);
+              $sql_sum_value ->execute();
+              $sum = $sql_sum_value->fetchAll()[0]['value_sum'] + $score;
+
+              $sql_count_command = "SELECT COUNT(Score) AS value_count FROM FinalRating WHERE ReID = $reid";
+              $sql_count_value = $connection->prepare($sql_count_command);
+              $sql_count_value ->execute();
+              $count = $sql_count_value->fetchAll()[0]['value_count'] + 1;
+              $ave = number_format($sum/$count);
+
+              $sql_update_ave = "UPDATE RatingResult
+                                 SET AverageScore = $ave, NumOfUsers = $count
+                                 WHERE RID = $rid";
+              $connection->prepare($sql_update_ave)->execute();
+
+              $sql_insert = "INSERT INTO FinalRating VALUES ('". $_SESSION['username'] ."', $reid, $rid, $score)";
+              $connection->prepare($sql_insert)->execute();
+          }else {
+              //else INSERT AverageScore, NumOfUsers(1) FROM RatingResult
+                //INSERT FinalRating(remember to query RID using MAX(RID))
+              $sql_insert_command = "INSERT INTO RatingResult (AverageScore, NumOfUsers)
+                                     VALUES ($score, 1)";
+              $connection->prepare($sql_insert_command)->execute();
+              $sql_insert = "INSERT INTO FinalRating
+                             VALUES ('". $_SESSION['username'] ."', $reid, (SELECT MAX(RID) FROM RatingResult),$score)";
+              $connection->prepare($sql_insert)->execute();
+          }
+
+
+
+      } else {
+          $sql_update_command = "UPDATE FinalRating
+                                     SET score = $score
+                                 WHERE ReID = $reid AND UserName = '" . $_SESSION['username'] . "'";
+          $sql_update_value = $connection->prepare($sql_update_command);
+          $sql_update_value ->execute();
+
+          //grabe the sums of score and amount of users WHERE ReID = FinalRating.$ReID
+          $sql_sum_command = "SELECT SUM(Score) AS value_sum FROM FinalRating WHERE ReID = $reid";
+          $sql_sum_value = $connection->prepare($sql_sum_command);
+          $sql_sum_value ->execute();
+          $sum = $sql_sum_value->fetchAll()[0]['value_sum'];
+
+          $sql_count_command = "SELECT COUNT(Score) AS value_count FROM FinalRating WHERE ReID = $reid";
+          $sql_count_value = $connection->prepare($sql_count_command);
+          $sql_count_value ->execute();
+          $count = $sql_count_value->fetchAll()[0]['value_count'];
+
+          //update AverageScore in RatingResult
+          $aveScore = number_format($sum/$count);
+          $sql_update_ave = "UPDATE RatingResult
+                             SET AverageScore = $aveScore
+                             WHERE RID = (SELECT DISTINCT RID FROM FinalRating WHERE ReID = $reid)";
+          $connection->prepare($sql_update_ave)->execute();
+      }
+    } catch(PDOException $error) {
+      echo $sql_get_rating . "<br>" . $error->getMessage();
+    }
+}
+if (isset($_GET['ReID'])) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+    $ReID = $_GET['ReID'];
+    $sql = "SELECT r.ReID, r.SkillLevel, r.Name, r.PrepTime, r.CookTime, i.Instructions, i.ServingSize FROM Recipe AS r, Instruction AS i, RecipeTime AS rt WHERE r.InstructionID = i.InsID AND rt.PrepTime = r.PrepTime AND rt.CookTime = r.CookTime AND r.ReID =:ReID";
+    $statement = $connection->prepare($sql);
+    $statement->bindValue(':ReID', $ReID);
+    $statement->execute();
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+} else {
+    echo "Something went wrong!";
+    exit;
+}
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) : ?>
+  Thank you for your rating.
+<?php endif; ?>
+
+ <h2>Rate a Recipe</h2>
+
+<form method="post" class>
+    <?php foreach ($user as $key => $value) : ?>
+      <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
+      <input type="text" name="<?php echo $key; ?>" ReID="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key = 'readonly'); ?>>
+    <?php endforeach; ?>
+    <label> Very Satisfied = 5 <br>
+            Satisfied = 4<br>
+            Neutral = 3<br>
+            Dissatisfied = 2<br>
+            Very Dissatisfied = 1</label>
+    <select name="score" id="score">
+      <option value=5>5</option>
+      <option value=4>4</option>
+      <option value=3>3</option>
+      <option value=2>2</option>
+      <option value=1>1</option>
+    </select>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
---
+++ rateRecipes.php
@@ -0,0 +1,82 @@
+<?php include "templates/header.php"; ?>
+<?php
+if(session_id() == '') {
+  session_start();
+}
+
+$username1 = "";
+
+require "./config.php";
+require "./common.php";
+
+if(!empty($_SESSION['username'])) {
+    $username1 = $_SESSION['username'];
+    echo "Currently logged in as: ". $username1 . "<br>";
+} else {
+    echo 'no session';
+}
+
+try  {
+
+    $connection = new PDO($dsn, $username, $password, $options);
+
+    $sql = "SELECT * FROM Recipe AS r, Instruction AS i, RecipeTime AS rt
+                    WHERE r.InstructionID = i.InsID AND rt.PrepTime = r.PrepTime
+                    AND rt.CookTime = r.CookTime";
+
+
+    $statement = $connection->prepare($sql);
+    $statement->bindParam(':username1', $username1, PDO::PARAM_STR);
+    $statement->execute();
+
+    $result = $statement->fetchAll();
+
+} catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+}
+
+?>
+<?php
+
+    if ($result && $statement->rowCount() > 0) { ?>
+        <h2>My Recipes</h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>ReID</th>
+                    <th>Skill Level</th>
+                    <th>Name</th>
+                    <th>PrepTime</th>
+                    <th>CookTime</th>
+                    <th>TotalTime</th>
+                    <th>Instruction</th>
+                    <th>Serving Size</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php foreach ($result as $row) { ?>
+            <tr>
+                <td><?php echo $row["ReID"]; ?></td>
+                <td><?php echo $row["SkillLevel"]; ?></td>
+                <td><?php echo $row["Name"]; ?></td>
+                <td><?php echo $row["PrepTime"]; ?></td>
+                <td><?php echo $row["CookTime"]; ?></td>
+                <td><?php echo $row["TotalTime"]; ?></td>
+                <td><?php echo $row["Instructions"]; ?></td>
+                <td><?php echo $row["ServingSize"]; ?></td>
+                <td><a href="rate-single.php?ReID=<?php echo escape($row["ReID"]); ?>">Rate</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+    <?php } else { ?>
+        <blockquote>No results found for <?php echo escape($_SESSION['username']); ?>.</blockquote>
+    <?php }
+ ?>
+
+<br>
+<a href="index.php">Back to home</a>
+
+
+<?php include "templates/footer.php"; ?>
